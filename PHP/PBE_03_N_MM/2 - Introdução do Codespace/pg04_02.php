a6d82f2c6aadfbc0847b4bd93252fe24b491a632
leandro-msp <leandromarcjt@gmail.com>
2026-08-12 20:45:25 -0300

    Material PBE_TURMA3_MM - PHP

---
+++ PHP/PBE_03_N_MM/2 - Introdução do Codespace/pg04_02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Página 4.2</title>
+</head>
+<body>
+    <h1>Pg04_02</h1>
+    <!-- <h2>Segunda importância</h2> ctrl+; é comando para inserir comentário, independente da linguagem (php,html)--> 
+    <?php
+    phpinfo(); //Verificar status de configuração
+   
+    /*
+    campo onde eu posso add un comentário mais extenso.
+    */
+    ?>
+</body>
+</html>
