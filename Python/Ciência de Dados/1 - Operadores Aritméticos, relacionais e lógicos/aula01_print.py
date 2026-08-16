35a2238344d17e7d389dec2cfed7681c6543a3a5
leandro-msp <leandromarcjt@gmail.com>
2026-08-15 23:34:13 -0300

    print

---
+++ Python/Ciência de Dados/1 - Operadores Aritméticos, relacionais e lógicos/aula01_print.py
@@ -0,0 +1,11 @@
+#função print
+
+d = "Uninove"
+y = "Leandro"
+e = 10
+print ("Bem vindo ao mundo Phyton")
+print (e)
+print (y)
+print (d)
+
+print("Bem vindo ao mundo Python com "+y+"professor da " + d , " que é ", e)
