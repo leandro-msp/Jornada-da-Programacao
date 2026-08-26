f487f3b66adba74ed58350abe30a11a489b65795
leandro-msp <leandromarcjt@gmail.com>
2026-08-26 17:56:14 -0300

    transicao

--- Python/Ciência de Dados/1 - Operadores Aritméticos, relacionais e lógicos/aula01_input.py
+++
@@ -1,4 +0,0 @@
-print("Dados do Aluno")
-nome = input("Informe seu nome: ")
-idade = input ("Informe sua idade : ")
-print (nome,"você tem ", idade, "anos")
--- Python/Ciência de Dados/1 - Operadores Aritméticos, relacionais e lógicos/aula01_variaveis.py
+++
@@ -1,8 +0,0 @@
-x = "UNINOVE"
-a = 10
-b = 8.6
-c = False
-print(type(x)) # mostrar o tipo de dado da variável
-print(type(a))
-print(type(b))
-print(type(c))
