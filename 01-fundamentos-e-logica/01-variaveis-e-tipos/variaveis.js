65cb0d0c795bf157add7dbb5adfb15acd325bd6b
leandro-msp <leandromarcjt@gmail.com>
2026-08-25 22:39:39 -0300

    variável e tipo de dados em JS

---
+++ 01-fundamentos-e-logica/01-variaveis-e-tipos/variaveis.js
@@ -0,0 +1,18 @@
+let jogo = "Minecraft";
+let lançamento = 2009;
+let empresa = "Mojang";
+
+const game = `O ${jogo} foi lançado em ${lançamento} pela ${empresa}.`;
+console.log(game);
+
+let cidade = "Barra Funda"
+console.log(cidade);
+
+cidade ="Carapicuíba";
+console.log(cidade);
+
+let nota1 = 10;
+let nota2 = 8;
+let media = (nota1+nota2)/2;
+
+console.log(media);
