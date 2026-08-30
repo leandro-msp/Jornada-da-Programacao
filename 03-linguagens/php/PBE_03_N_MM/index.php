c06cad176d44365c3a7b409a17a892f1a1d1daae
leandro-msp <leandromarcjt@gmail.com>
2026-08-29 23:23:10 -0300

    att

--- 03-linguagens/php/PBE_03_N_MM/index.php
+++ 03-linguagens/php/PBE_03_N_MM/index.php
@@ -6,27 +6,27 @@
     <title>Página Inicial - Programação Back-End </title>
 </head>
 <body>
-    <h1>Estudando PHP</h1>
+    <h1>PROGRAMAÇÃO BACK-END c/ PHP - Turma 3MM</h1>
     <hr>
     <?php
     echo "<h2>Páginas do Material</h2>";
     ?>
-    <a href="/1 - Código Inicial/pg1.php">1 - Primeira escrita</a><br>
-    <a href="/2 - Introdução do Codespace/pg04_01.php">2.1 Introdução no Codespace</a><br>
-    <a href="/2 - Introdução do Codespace/pg04_02.php">2.2 Introdução no Codespace</a><br>
-    <a href="/3 - Tipos de Dados/aula05_01.php">3.1 Tipos de Dados(Int,Float,String)</a><br>
-    <a href="/3 - Tipos de Dados/aula05_02.php">3.2 Tipos de Dados(Constante)</a><br>
-    <a href="/3 - Tipos de Dados/ldc01_01.php">3.3 Lição de Casa 01 - Tipos de Dados Concatenação</a><br>
-    <a href="/3 - Tipos de Dados/ldc01_02.php">3.4 Lição de Casa 02 - Manipulação de Dados, Constantes</a><br>
-    <a href="/4 - Criando Formulário/aula06_01.php">4.1Criação de Formulário</a><br>
-    <a href="/4 - Criando Formulário/ldc02_form01_01.php">4.2 Lição de Casa - Formulário</a><br>
-    <a href="/5 - Operadores/aula07_01.php">5.1 Operados Aritiméticos, Atribuição, String, Increment e Decremento</a><br>
-    <a href="/5 - Operadores/aula07_02.php">5.2 Operados Lógicos</a><br>
-    <a href="/5 - Operadores/aula08_01.php">5.3 Explicação 2 dos Operadores</a><br>
-    <a href="/6 - Estrutura Condicional/aula09_01.php">6.1 Estrutura de Condição (If e Else)</a><br>
-    <a href="/6 - Estrutura Condicional/aula09_02.php">6.2 Estrutura de Condição (switch/Case)</a><br>
-    <a href="/7 - Estrutura de Looping/aula10_01.php">7.1 Estrutura de Looping (While/Do/For)</a><br>
-    <a href="/8 - Arrays/aula11_01.php">8.1 Arrays</a><br>
-    <a href="/9 - foreach/aula12_01.php">9.1 Foreach</a><br>
-    <a href="/10 - Sessão/aula13_01.php">9.1 Sessão</a><br>
+    <a href="./1 - Código Inicial/pg1.php">1 - Primeira código</a><br>
+    <a href="./2 - Introdução do Codespace/pg04_01.php">2.1 Introdução no Codespace</a><br>
+    <a href="./2 - Introdução do Codespace/pg04_02.php">2.2 Introdução no Codespace</a><br>
+    <a href="./3 - Tipos de Dados/aula05_01.php">3.1 Tipos de Dados(Int,Float,String)</a><br>
+    <a href="./3 - Tipos de Dados/aula05_02.php">3.2 Tipos de Dados(Constante)</a><br>
+    <a href="./3 - Tipos de Dados/ldc01_01.php">3.3 Lição de Casa 01 - Tipos de Dados Concatenação</a><br>
+    <a href="./3 - Tipos de Dados/ldc01_02.php">3.4 Lição de Casa 02 - Manipulação de Dados, Constantes</a><br>
+    <a href="./4 - Criando Formulário/aula06_01.php">4.1Criação de Formulário</a><br>
+    <a href="./4 - Criando Formulário/ldc02_form01_01.php">4.2 Lição de Casa - Formulário</a><br>
+    <a href="./5 - Operadores/aula07_01.php">5.1 Operados Aritiméticos, Atribuição, String, Increment e Decremento</a><br>
+    <a href="./5 - Operadores/aula07_02.php">5.2 Operados Lógicos</a><br>
+    <a href="./5 - Operadores/aula08_01.php">5.3 Explicação 2 dos Operadores</a><br>
+    <a href="./6 - Estrutura Condicional/aula09_01.php">6.1 Estrutura de Condição (If e Else)</a><br>
+    <a href="./6 - Estrutura Condicional/aula09_02.php">6.2 Estrutura de Condição (switch/Case)</a><br>
+    <a href="./7 - Estrutura de Looping/aula10_01.php">7.1 Estrutura de Looping (While/Do/For)</a><br>
+    <a href="./8 - Arrays/aula11_01.php">8.1 Arrays</a><br>
+    <a href="./9 - foreach/aula12_01.php">9.1 Foreach</a><br>
+    <a href="./10 - Sessão/aula13_01.php">9.1 Sessão</a><br>
     <a href="ldc_impPar.php">Lição de casa - Par ou Ímpar</a><br>
