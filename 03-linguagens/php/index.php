a777491ed95a87fb8352c6ff1975e2593627105e
leandro-msp <leandromarcjt@gmail.com>
2026-08-29 23:23:17 -0300

    main

---
+++ 03-linguagens/php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Jornada PHP</title>
+</head>
+<body>
+    <h1>Exercícios e Estudos de PHP</h1>
+    <ul>
+        <li><a href="./PBE_03_N_MM/index.php">Programação Back-End (Estudos Acadêmicos)</a></li>
+    </ul>
+</body>
+</html>
