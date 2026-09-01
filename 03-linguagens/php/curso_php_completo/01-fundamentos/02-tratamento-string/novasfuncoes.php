cd494e5b1a2209f982fcefb0c0fc9bebd42e6f4f
leandro-msp <leandromarcjt@gmail.com>
2026-09-01 15:34:11 -0300

    tratamento string

--- 03-linguagens/php/curso_php_completo/01-fundamentos/02-tratamento-string/novasfuncoes.php
+++ 03-linguagens/php/curso_php_completo/01-fundamentos/02-tratamento-string/novasfuncoes.php
@@ -1,30 +1,36 @@
 <?php
 $mensagem = "Curso completo de PHP";
 
-# Confirmar se a string possui o valor informado -> função str_contains > retorna valor boolean (true, false)
+//todas as funções abaixo retorna um valor booleano (true/false)
 
-$possui = str_contains($mensagem,'completo');  // str_contains requer dois parâmetros: onde será a pesquisa e o valor a ser verificado
-echo "Possui: ".$possui."\n"; // 1(boolean) = true 
-echo "GETTYPE: ".gettype($possui)."\n"; // gettype mostra apenas o tipo, mas não o valor (true ou false)
-// neste cenário, conforme a necessidade do sistema a variável $possui pode ser utilizada para operações lógicas(condição, comparação).
+# Confirmar se a string possui o valor informado 
 
-var_dump($possui); // var_dump é uma função de depuração que exibe o tipo de dado, o valor e tamanho.
+$possui = str_contains($mensagem,'completo');  
+echo "Possui: ".$possui."\n"; 
+echo "GETTYPE: ".gettype($possui)."\n"; // <-- get não mostra o valor, somente o tipo
+
+var_dump($possui); // <- unção de depuração que exibe o tipo de dado, o valor , e o tamanho
 
 //método curto 
 echo "Str Contains: ";
-var_dump(str_contains($mensagem,'completo'));  // função case_sensitive
+var_dump(str_contains($mensagem,'completo')); 
 
-# Confirmar se a string começa com valor informado -> função str_start_with > retorna valor boolean (true, false)
+# Confirmar se a string começa com valor informado 
 echo "Start With: ";
-var_dump(str_starts_with($mensagem,'Curso')); // verifica se a string começa com o valor informado -> função case_sensitive
+var_dump(str_starts_with($mensagem,'Curso')); 
 
 
-# Confirmar se a string termina com o valor informado -> função str_ends_with > retorna valor boolean (true, false)
+# Confirmar se a string termina com o valor informado 
 echo "Ends With: ";
-var_dump(str_ends_with($mensagem,'PHP')); // verfica se a string termina com o valor informado -> função case_sensitive
+var_dump(str_ends_with($mensagem,'PHP')); 
 
-#método com formatação no var_dump para não cair no case sensitive
+#método com formatação para não cair no case sensitive
 
 // ex.:
-echo "Ends With -> php minúsculo: ";
+echo "\nEnds With -> php minúsculo:\n";
+echo "var_dump+strtolower: ";
 var_dump(str_ends_with(strtolower($mensagem),'php'));
+
+$termina = str_ends_with(strtolower($mensagem),'php');
+echo "Gettype: ".gettype($termina)."\n";
+var_dump($termina);
