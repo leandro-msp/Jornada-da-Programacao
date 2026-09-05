775da6e1ffa911a0564a1cab48406fde767e58f2
leandro-msp <leandromarcjt@gmail.com>
2026-09-05 19:47:52 -0300

    index

--- php/index.php
+++ php/index.php
@@ -4,10 +4,29 @@
     <meta charset="UTF-8">
     <title>Jornada PHP</title>
 </head>
-<body>
-    <h1>Curso PHP COMPLETO + Projetos</h1>
+<body style="background-color: #D7EBF1;">
+    <h1 style="color:blueviolet">Curso PHP Completo + Projetos</h1>
     <ul>
-        <li><a href="./PBE_03_N_MM/index.php">Programação Back-End (Estudos Acadêmicos)</a></li>
+        <h2>Fundamentos</h2>
+         <h4 style="color: red;">AVISO:<br>Tópicos "Variáveis, Tratamento String, e Arrays" a quebra de linha está formatada para saídas no terminal(exceto o arquivo de constantes)</h4>
+
+        <h3>Variáveis e Constantes</h3>
+        <li><a href="./01-fundamentos/01-variaveis-e-constantes/variaveis.php">Variáveis</a></li>
+        <li><a href="./01-fundamentos/01-variaveis-e-constantes/constantes.php">Constantes</a></li>
+
+        <h3>Tratamento String</h3>
+        <li><a href="./01-fundamentos/02-tratamento-string/string1.php">Tratamento de String(Trim)</a></li>
+        <li><a href="./01-fundamentos/02-tratamento-string/string2.php">Tratamento de String(Upper,Lower,ucfirst,ucwords)</a></li>
+        <li><a href="./01-fundamentos/02-tratamento-string/novasfuncoes.php">Tratamento de String(Contains,Start_With,Ends_With)</a></li>
+        
+        <h3>Arrays</h3>
+        <li><a href="./01-fundamentos/03-arrays/arrays.php">Arrays Indexado e associativo</a></li>
+        <li><a href="./01-fundamentos/03-arrays/arrays2.php">Manipulando Arrays</a></li>
+
+        <h3>Operadores</h3>
+        <li><a href="./01-fundamentos/04-operadores/operadores.php">Operadores Relacionais</a></li>
+        <li><a href="./01-fundamentos/04-operadores/logicos.php">Operadores Lógicos</a></li>
+
     </ul>
 </body>
 </html>
